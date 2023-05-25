dff2f90cbaa62947f0db155475efdf2cbd21333c
Noah Beito <noah.beito@gmail.com>
2023-05-25 11:57:36 -0700

    update schema to store dates as BIGINTS and corrected columns of photos table. Updated columns in COPY photos in import.sql. Schema and import files now work to load data into all three tables

--- import.sql
+++ import.sql
@@ -1,1 +1,8 @@
+-- psql command to run this file in terminal:
+-- \i /Users/noahbeito/Desktop/RFP2303/QuestionsAPI/import.sql
+
 \COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful) FROM '/Users/noahbeito/Desktop/RFP2303/QuestionsAPI/data/questions.csv' DELIMITER ',' CSV HEADER;
+
+\COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) FROM '/Users/noahbeito/Desktop/RFP2303/QuestionsAPI/data/answers.csv' DELIMITER ',' CSV HEADER;
+
+\COPY photos(id, answer_id, url) FROM '/Users/noahbeito/Desktop/RFP2303/QuestionsAPI/data/answers_photos.csv' DELIMITER ',' CSV HEADER;
