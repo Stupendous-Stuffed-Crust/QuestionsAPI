f3f78156d95be8be55a69f4a6742c989036402fe
Noah Beito <noah.beito@gmail.com>
2023-05-25 22:30:01 -0700

    added db connection

---
+++ server/db.js
@@ -0,0 +1,16 @@
+// Connect to PostgreSQL database and export connection instance via db
+
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+client.connect();
+
+module.exports.client = client;
